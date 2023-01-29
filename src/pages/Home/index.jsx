49e4034b442282ca64f1c23ab739a35885043f91
HicaroD <hdanrlley1@gmail.com>
2023-01-29 10:59:32 -0300

    fix(typos)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             <p>
                 Hello, everyone! My name is <b>Hícaro Dânrlley</b> and I'm a <b>Computer Science student </b>
                 and a <b>software developer</b>. I started my
-                journey in coding three years ago with C programming language.
+                journey in coding two years ago with C programming language.
                 Actually, at first, I wanted to be a lawyer, but a great friend of mine told me about
                 a Harvard course called <a href="https://www.edx.org/cs50">CS50</a> and
                 that changed my life forever (Thanks, Alice ♥).
@@ -15,9 +15,9 @@ export default function Home() {
             </p>
             <br></br>
             <p>
-                Nowadays I work as a Flutter developer. In my spare time, I like writing open source
+                I work as a Flutter developer. In my spare time, I like writing open source
                 software, especially software for improving the productivity of other developers.
-                I would love to contribue to other open source projects, so I can help the community while
+                I would love to contribute to other open source projects, in that way I can help the community while
                 improving my skills.
             </p>
         </div>
