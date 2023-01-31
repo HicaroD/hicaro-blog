fb1cd7f5844c7e76fe4e78c2708c985456aa3188
HicaroD <hdanrlley1@gmail.com>
2023-01-31 10:48:58 -0300

    feat(#2): implementing project cards

---
+++ src/api/projects.tsx
@@ -0,0 +1,26 @@
+export default [ 
+    {
+        title: "TodoAsIssue",
+        description: "CLI for converting a TODO list into a list of issues on your repository",
+        link: "https://github.com/HicaroD/TodoAsIssue",
+        tech: ["dart"],
+    },
+    {
+        title: "PullRequests",
+        description: "CLI for creating pull requests easily",
+        link: "https://github.com/HicaroD/pull-requests",
+        tech: ["python"],
+    },
+    {
+        title: "Conway's Game of Life",
+        description: "My own implementation of Conway's of Life",
+        link: "https://github.com/HicaroD/game-of-life",
+        tech: ["cpp"],
+    },
+    {
+        title: "Chip8",
+        description: "My own implementation of Chip 8 interpreter",
+        link: "https://github.com/HicaroD/chip8-rs",
+        tech: ["rust"],
+    },
+];
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -3,5 +3,13 @@ import projects from "../../api/projects";
 
 export default function Projects() {
     return <div className="projects">
+        {
+            projects.map((project, index) => {
+                return <div id="project" key={index}>
+                    <h2 id="project-name">{project.title}</h2>
+                    <p>{project.description}</p>
+                </div>
+            })
+        }
     </div>;
 }
