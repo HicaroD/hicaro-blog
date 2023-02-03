920e9be670aa72070e9f952878428dca0e916ace
HicaroD <hdanrlley1@gmail.com>
2023-02-03 10:17:14 -0300

    prettier on `Navbar`

--- src/pages/Layout/components/Topbar/Navbar/index.jsx
+++ src/pages/Layout/components/Topbar/Navbar/index.jsx
@@ -2,15 +2,27 @@ import { Link } from "react-router-dom";
 import styles from "./style.module.css";
 
 export default function Navbar() {
-    return <ul id={styles.navbar}>
-        <li>
-            <Link to="/">Projects</Link>
-        </li>
-        <li>
-            <Link to={{ pathname: "https://drive.google.com/drive/folders/1HEgd8xXOdbcE1ve6Uhzkxa3vlJ06AfjY" }} target="_blank">Resume</Link>
-        </li>
-        <li>
-	    <a href="mailto:hdanrlley1@gmail.com" target="_blank">E-mail</a>
-        </li>
-    </ul>;
+	return (
+		<ul id={styles.navbar}>
+			<li>
+				<Link to="/">Projects</Link>
+			</li>
+			<li>
+				<Link
+					to={{
+						pathname:
+							"https://drive.google.com/drive/folders/1HEgd8xXOdbcE1ve6Uhzkxa3vlJ06AfjY",
+					}}
+					target="_blank"
+				>
+					Resume
+				</Link>
+			</li>
+			<li>
+				<a href="mailto:hdanrlley1@gmail.com" target="_blank">
+					E-mail
+				</a>
+			</li>
+		</ul>
+	);
 }
