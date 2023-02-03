2e400e0ac65523222bef8d0c7dfdcfd0a871159f
HicaroD <hdanrlley1@gmail.com>
2023-02-03 14:08:24 -0300

    feat: removing old ideas for project folder

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,20 +1,7 @@
-import projects from "../../api/projects";
-import ProjectCard from "./components/ProjectCard";
 import style from "./style.module.css";
 
 export default function Projects() {
-	return (
-		<div className={style.projects}>
-			{projects.map((project, index) => {
-				return (
-					<ProjectCard
-						index={index}
-						title={project.title}
-						description={project.description}
-						link={project.link}
-					/>
-				);
-			})}
-		</div>
-	);
+	return <div className={style.projects}>
+		<p></p>
+	</div>;
 }
